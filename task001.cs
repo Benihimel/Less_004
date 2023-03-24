549c100090f863658254b4efcb232f5eb50a8d79
Benihimel <benihimekugutsu@gmail.com>
2023-03-24 18:56:09 -0400

    task001

---
+++ task001.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Enter number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int DegreeNumA(int A)
+{
+    int NumA = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        NumA = NumA * A;
+    }
+    return NumA;
+}
+Console.WriteLine($"The number {A} to the power of {B} is: {DegreeNumA(A)}");
