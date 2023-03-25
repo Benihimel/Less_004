f3011aa9a3f8eba2b87204425814f7b322619a3a
Benihimel <127148808+Benihimel@users.noreply.github.com>
2023-03-24 20:47:06 -0400

    Add task003

---
+++ task003.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int N = 8;
+int[] Array(int x)
+{
+    int[] arr = new int[x];
+    Random rand = new Random();
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = rand.Next(-1000,1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] list)
+{
+    // int[]PrintList = Array();
+    for (int i = 0; i < list.Length; i++)
+    {
+        Console.Write($"{list[i]} ");
+    }
+}
+PrintArray(Array(N));
